3c2a868ab442500b170055e9ee73a6f6d663ceea
taclayton0594 <taylor2@alumni.stanford.edu>
2024-07-22 15:44:53 -0500

    Added base logging and exception handling code.

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys # functions and variables for manipulating different parts of Python runtime env
+import logging
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    err_msg = "Error occured in Python script [{0}] line number [{1}] error message [{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error))
+    
+    return err_msg
+
+class CustomException(Exception):
+    def __init__(self,err_msg,err_det:sys):
+        super().__init__(err_msg)
+        self.err_msg=error_message_detail(err_msg,error_detail=err_det)
+
+    def __str__(self):
+        return self.err_msg
+    
+if __name__=="__main__":
+    logging.info("Logging started.")
+    try:
+        a = 1/ 0
+    except Exception as e:
+        logging.info("Divide by zero.")
+        raise CustomException(e,sys)
